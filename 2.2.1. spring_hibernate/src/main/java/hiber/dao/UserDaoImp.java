8d996f9ef14b6f5ec51336b381858b303805c8fd
Sharifyanov Marsel <sharifyanovmarsel@gmail.com>
2025-01-09 16:39:05 +0500

    problems with method findUserByCar

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,6 +2,7 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,19 +12,34 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User findUserByCar(int series, String model) {
+        Query query = sessionFactory.getCurrentSession().createQuery("from User where Car.series = :series and Car.model = :model");
+        return (User) query.setParameter("series", series).setParameter("model", model).uniqueResult();
+    }
 
 }
+
+/*
+String hql = "FROM User WHERE name = :paramName";
+Query query = session.createQuery(hql);
+query.setParameter("paramName", "Alex");
+
+// Получаем единственный результат
+User user = (User) query.uniqueResult();
+ */
